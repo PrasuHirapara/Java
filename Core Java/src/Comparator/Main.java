79126c189a9654a28455f668e8c0f584b8cbf647
Dell <prasuhirpara@gmail.com>
2024-10-20 00:26:39 +0530

    comparator

--- Core Java/src/Comparator/Main.java
+++ Core Java/src/Comparator/Main.java
@@ -1,4 +1,23 @@
 package Comparator;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student(90.4f, 12);
+        Student student2 = new Student(87.9f, 13);
+
+        if(student1.compareTo(student2) < 0){
+            System.out.println("Student2 has higher marks");
+        }else {
+            System.out.println("Student has lower marks");
+        }
+
+        Student[] students = {new Student(88.5f, 101), new Student(78.0f, 99), new Student(92.0f, 102)};
+        Arrays.sort(students, (a, b) -> a.toString().compareTo(b.toString()));
+
+        System.out.println(Arrays.toString(students));
+    }
 }
--- Core Java/src/Comparator/Student.java
+++ Core Java/src/Comparator/Student.java
@@ -1,5 +1,21 @@
 package Comparator;
 
-public class Student {
+public class Student implements Comparable<Student> {
+    int rollno;
+    float marks;
 
+    public Student(float marks, int rollno) {
+        this.marks = marks;
+        this.rollno = rollno;
+    }
+
+    @Override
+    public String toString() {
+        return rollno + " " + marks;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return (int) ((int) this.marks - o.marks);
+    }
 }
