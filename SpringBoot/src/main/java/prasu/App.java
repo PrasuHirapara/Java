0e30fa7aa9c99d6747963f713cbda67552f3c715
Dell <prasuhirpara@gmail.com>
2024-12-14 18:16:44 +0530

    set up

--- SpringBoot/src/main/java/prasu/App.java
+++ SpringBoot/src/main/java/prasu/App.java
@@ -1,13 +1,13 @@
-package org.springboot;
+package prasu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(App.class, args);
 		System.out.println("Hello World");
 	}
 }
