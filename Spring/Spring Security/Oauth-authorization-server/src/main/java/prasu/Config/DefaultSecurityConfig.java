f570c3ba5df017cd2ed313f38588066d94e35574
Dell <prasuhirpara@gmail.com>
2024-12-25 19:59:06 +0530

    setting up OAuth2

--- Spring/Spring Security/Oauth-authorization-server/src/main/java/prasu/Config/DefaultSecurityConfig.java
+++ Spring/Spring Security/Oauth-authorization-server/src/main/java/prasu/Config/DefaultSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import prasu.*;
 import prasu.Service.CustomAuthenticationProvider;
 
 @Configuration
