787c5f3577e82df599b5bfaf84004f3642194cec
Dell <prasuhirpara@gmail.com>
2025-01-03 21:48:21 +0530

    changes

--- Project/CryptoFlow/backend/src/main/java/com/cryptoflow/response/AuthResponse.java
+++ Project/CryptoFlow/backend/src/main/java/com/cryptoflow/response/AuthResponse.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class AuthResponse {
 
     private String jwt;
-    private boolean status;
     private String message;
-    private boolean isTwoFactorAuthEnabled;
     private String session;
+    private boolean status;
+    private boolean isTwoFactorAuthEnabled;
 }
