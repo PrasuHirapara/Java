a5b2409577b682b44f3e989fef020d863f935511
Dell <prasuhirpara@gmail.com>
2025-01-04 19:04:05 +0530

    Login functionality successful

--- Project/CryptoFlow/backend/src/main/java/com/cryptoflow/controller/AuthController.java
+++ Project/CryptoFlow/backend/src/main/java/com/cryptoflow/controller/AuthController.java
@@ -3,13 +3,16 @@ package com.cryptoflow.controller;
 import com.cryptoflow.config.JwtProvider;
 import com.cryptoflow.entity.User;
 import com.cryptoflow.response.AuthResponse;
+import com.cryptoflow.service.CustomUserDetailService;
 import com.cryptoflow.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,9 +29,12 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
     @PostMapping("/signup")
-    public ResponseEntity<AuthResponse> saveUser(@RequestBody User user) throws Exception {
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    public ResponseEntity<AuthResponse> signup(@RequestBody User user) throws Exception {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         User isEmailExist = userService.findByEmail(user.getEmail());
         if (isEmailExist != null) {
@@ -52,5 +58,39 @@ public class AuthController {
         res.setMessage("Success");
 
         return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody User user) throws Exception {
+
+        String email = user.getEmail();
+        String password = user.getPassword();
+
+        Authentication auth =  authenticate(email, password);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("Login Success");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String email, String password) {
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(email);
+
+        if(userDetails == null) {
+            throw new BadCredentialsException("invalid email or password");
+        }
+
+        if(!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
     }
 }
